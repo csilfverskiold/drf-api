3a5a92f0b3bb702bc6c2a8bdb29bdc31c09881cd
csilfverskiold <christian.silfverskiold@gmail.com>
2023-02-09 16:55:48 +0000

    Edit env.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)rklom_j#em3ay)-t$x)7-cw_^%+!0ztz6m45j%)+mzjli_n51'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
