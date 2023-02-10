003d1645309e66d367cc3b8ef01ea06a988ba251
csilfverskiold <christian.silfverskiold@gmail.com>
2023-02-10 08:55:05 +0000

    Add serializer to ProfileList view

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
+from .serializers import ProfileSerializer
 # ^ See cheat sheet for comments on import ^
 
 
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        return Response(profiles)
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+# "Our serializer has taken the Python  data and converted it
+# into JSON, which is ready for the front end content to use!"
