d1921625a05ec95b93275e61886fe5f9eeb11eb6
csilfverskiold <christian.silfverskiold@gmail.com>
2023-02-10 09:51:57 +0000

    Create ProfileDetail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -13,3 +15,33 @@ class ProfileList(APIView):
 
 # "Our serializer has taken the Python  data and converted it
 # into JSON, which is ready for the front end content to use!"
+
+
+class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
+# "If we explicitly set the serializer_class attribute  on our
+# ProfileDetail view, the rest framework will automatically
+# render a form for us, based on  the fields we defined in our
+# ProfileSerializer."
+
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    # GET
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    # PUT
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
