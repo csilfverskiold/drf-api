c9816bebe11030da0d0fade4b8f48310aebd7056
csilfverskiold <christian.silfverskiold@gmail.com>
2023-02-13 09:19:51 +0000

    Add posts, followers and following counters to Profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -9,8 +10,37 @@ class ProfileList(generics.ListAPIView):
     List all profiles.
     No create view as profile creation is handled by django signals.
     """
-    queryset = Profile.objects.all()
+
+    """To be able to sort these fields,  first we will have to define them,
+    using the 'annotate' function  on the List view queryset.
+    Let's go then to profiles' views.py  file. First, we'll import the Count
+    class to count model instances. We'll also need  rest_framework's filters
+    to order our profiles. Now we are going to remove the dot-all function
+    from our queryset and modify it using the annotate function instead.
+    The annotate function allows us to define extra fields to be added to the
+    queryset. In our case, we'll add fields to work  out how many posts and
+    followers a user has, and how many other users they're following."""
+    # queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    """Next, we'll need to create our filters. To make these fields sortable,
+    I'll set the filter_backends attribute to OrderingFilter.
+    I'll also need to set the ordering_fields  to the fields we just annotated,
+    namely posts, followers and following count."""
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
@@ -18,5 +48,9 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     Retrieve or update a profile if you're the owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
