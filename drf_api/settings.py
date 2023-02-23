0c277ad69ae311ebc16c7816f414e9ae01bf9a25
csilfverskiold <christian.silfverskiold@gmail.com>
2023-02-23 15:54:13 +0000

    Edit settings file

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -111,6 +111,15 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+         os.environ.get('CLIENT_ORIGIN')
+     ]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\gitpod\.io$",
+    ]
+
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'drf_api.urls'
